f8dbd49b978b2cc7aafb0eec8d2dd8f5653887ba
GabrielAdelino <gabriel.adelinodev@gmail.com>
2023-10-19 16:19:23 -0300

    configurando a controller

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -7,28 +7,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.DAO;
-/**
- * Servlet implementation class Controller
- */
-@WebServlet(urlPatterns = {"/Controller", "/master"})
+
+@WebServlet(urlPatterns = { "/Controller", "/master" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    DAO dao = new DAO();  
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Controller() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	DAO dao = new DAO();
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		/*TESTE DE CONEXÃO
-		dao.testeConexão();*/
+	public Controller() {
+		super();
 	}
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getServletPath();
+		System.out.println(action);
+
+		/*
+		 * TESTE DE CONEXÃO dao.testeConexão();
+		 */
+
+		if (action.equals("/master")) {
+			cadastro(request, response);
+		}
+	}
+
+	// Página Cadastro
+	protected void cadastro(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.sendRedirect("paginaCadastro.jsp");
+	}
 }
